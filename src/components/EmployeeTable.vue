7eb4408d7a686a038700817ce205021591e3ae36
AlzoPalzo <ali@ALI-Laptop.localdomain>
2019-10-23 00:24:06 +0100

    added delete button and functcionality

--- src/components/EmployeeTable.vue
+++ src/components/EmployeeTable.vue
@@ -1,21 +1,28 @@
 <template>
-  <div>
+  <div id="employee-table">
     <table>
       <thead>
         <tr>
           <th>Employee name</th>
           <th>Employee email</th>
+          <th>Actions</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="employee in employees" :key="employee.id">
           <td>{{ employee.name }}</td>
           <td>{{ employee.email }}</td>
+          <td>
+            <button>Edit</button>
+            <button @click="$emit('delete:employee', employee.id)">Delete</button>
+          </td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
+
+
 
 <script>
   export default {
@@ -26,4 +33,8 @@
   }
 </script>
     
-<style scoped></style>
+<style scoped>
+  button {
+    margin: 0 0.5rem 0 0;
+  }
+</style>
